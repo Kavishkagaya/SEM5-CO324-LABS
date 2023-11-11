03ac89f96ba43907a8962adcde08b00db0da9226
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-11-11 18:52:56 +0530

    finishing lab1

--- lab1/ex3.py
+++ lab1/ex3.py
@@ -6,19 +6,33 @@ from urllib.parse import quote
 def ddg_query(search_term: str, nr_results: int) -> list[str]:
     search_term = quote(search_term)
     query_link = f"https://duckduckgo.com/?q={search_term}&format=json"
-    with request.urlopen(query_link) as query:
-        body = query.read()
-        body = json.loads(body)
+    try:
+        with request.urlopen(query_link) as query:
+            body = query.read()
+            body = json.loads(body)
 
-        if "Results" in body:
-            results = body["Results"]
-            links = [result["FirstURL"] for result in results]
-            links = links[0:nr_results]
-            return links
-        else:
-            return []
+            if "Results" in body:
+                results = body["Results"]
+                links = [result["FirstURL"] for result in results]
+                links = links[0:nr_results]
+                return links
+            else:
+                return []
+    except Exception as e:
+        print(e)
+        return []
 
 
-result_links = ddg_query("University of Ruhuna", 1)
+def last_modified(url: str) -> str:
+    try:
+        req = request.Request(url, method="HEAD")
+        with request.urlopen(req) as query:
+            return query.headers["Last-Modified"]
+    except Exception as e:
+        print(e)
+        return "None"
+
+
+result_links = ddg_query("w3schools", 2)
 for link in result_links:
-    print(link)
+    print(link, last_modified(link))
